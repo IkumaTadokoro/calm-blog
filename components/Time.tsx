44c85b0717416550cc042e383220f3585c0fb5d9
ikuma-t <tadokorodev@gmail.com>
2022-06-13 22:43:33 +0900

    style: truncate title using CSS instead of scripts

--- components/Time.tsx
+++ components/Time.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 export default function Time({ dateTime }: { dateTime: string }) {
   return (
-    <time className="text-xs text-gray-300" dateTime={dateTime} title={dateTime}>
+    <time className="text-xs text-gray-300 truncate" dateTime={dateTime} title={dateTime}>
       {format(new Date(dateTime), "yyyy-MM-dd HH:MM:SS")}
     </time>
   );
